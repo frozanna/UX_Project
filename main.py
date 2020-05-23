012f48903b6e90941af26daf23cde83ef36ac570
frozanna <48160361+frozanna@users.noreply.github.com>
2020-05-23 12:41:55 +0200

    Init frame

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from window import Window
 
 if __name__ == "__main__":
     window = Window()
-    window()
+    window.run()
--- window.py
+++ window.py
@@ -1,13 +1,34 @@
 import pygame
+from pygame.locals import FULLSCREEN
 import sys
+import pygame.gfxdraw
+
+WINDOW_HEIGHT = 667
+WINDOW_WIDTH = 375
 
 class Window:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((750, 1334))
-        self.basic_font = pygame.font.Font('Pixeled.ttf', 25)
+        self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.screen.fill((255, 255, 255))
+        self.basic_font = pygame.font.Font('Fonts/OpenSans-Regular.ttf', 25)
+
+    def draw_frame(self):
+        phone_bar_img = pygame.image.load('Images/phone_bar.png')
+        self.screen.blit(phone_bar_img, (0, 0))
+
+        menu_bar_img = pygame.image.load('Images/menu_bar.png')
+        menu_h = menu_bar_img.get_height()
+        self.screen.blit(menu_bar_img, (0, WINDOW_HEIGHT - menu_h))
+
 
     def run(self):
-        pygame.init()
-        pygame.display.set_mode((750, 1334))
-        self.font = pygame.font.Font('Pixeled.ttf', 25)
+        self.draw_frame()
+        while True:
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit(0)
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = event.pos
