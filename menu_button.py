7328a3553d26ebbdb04a79e4af491cd82b681cac
frozanna <48160361+frozanna@users.noreply.github.com>
2020-05-23 17:49:27 +0200

    Make menu buttons work

--- menu_button.py
+++ menu_button.py
@@ -9,46 +9,59 @@ class MenuButton:
         start_x = 35
         space_x = 57
         self.active = active
-        if active:
-            self.y = WINDOW_HEIGHT - 63
-        else:
-            self.y = WINDOW_HEIGHT - 52
+        self.ac_y = WINDOW_HEIGHT - 63
+        self.nac_y = WINDOW_HEIGHT - 52
 
         if name == "activities":
             self.image_n = pygame.image.load('Images/activities_n.png')
             self.image_y = pygame.image.load('Images/activities_y.png')
-            self.x = start_x
+            self.ac_x = start_x
+            self.nac_x = self.ac_x - 10
         elif name == "planer":
             self.image_n = pygame.image.load('Images/planer_n.png')
             self.image_y = pygame.image.load('Images/planer_y.png')
-            self.x = start_x + space_x
+            self.nac_x = start_x + space_x
+            self.nac_x = start_x + space_x
         elif name == "food":
             self.image_n = pygame.image.load('Images/food_n.png')
             self.image_y = pygame.image.load('Images/food_y.png')
-            self.x = start_x + space_x * 2
+            self.nac_x = start_x + space_x * 2
+            self.nac_x = start_x + space_x * 2
         elif name == "sport":
             self.image_n = pygame.image.load('Images/sport_n.png')
             self.image_y = pygame.image.load('Images/sport_y.png')
-            self.x = start_x + space_x * 3
+            self.nac_x = start_x + space_x * 3
+            self.nac_x = start_x + space_x * 3
         elif name == "social":
             self.image_n = pygame.image.load('Images/social_n.png')
             self.image_y = pygame.image.load('Images/social_y.png')
-            self.x = start_x + space_x * 4
+            self.nac_x = start_x + space_x * 4
+            self.nac_x = start_x + space_x * 4
         elif name == "settings":
             self.image_n = pygame.image.load('Images/settings_n.png')
             self.image_y = pygame.image.load('Images/settings_y.png')
-            self.x = start_x + space_x * 5
+            self.nac_x = start_x + space_x * 5
+            self.nac_x = start_x + space_x * 5
         else:
             raise ValueError
 
+        self.image_n_rect = self.image_n.get_rect()
+        self.image_n_rect.x = self.nac_x
+        self.image_n_rect.y = self.nac_y
+        self.image_y_rect = self.image_y.get_rect()
+        self.image_y_rect.x = self.nac_x
+        self.image_y_rect.y = self.ac_y
+
     def draw(self):
         if self.active:
-            self.screen.blit(self.image_y, (self.x, self.y))
+            self.screen.blit(self.image_y, self.image_y_rect)
+            pygame.draw.rect(self.screen, (245, 245, 245), self.image_y_rect, 1)
         else:
-            self.screen.blit(self.image_n, (self.x, self.y))
+            self.screen.blit(self.image_n, self.image_n_rect)
+            pygame.draw.rect(self.screen, (245, 245, 245), self.image_n_rect, 1)
 
     def pressed(self, mouse_pos):
         if self.active:
-            return self.image_y.collidepoint(mouse_pos)
+            return self.image_y_rect.collidepoint(mouse_pos)
         else:
-            return self.image_n.collidepoint(mouse_pos)
+            return self.image_n_rect.collidepoint(mouse_pos)
--- window.py
+++ window.py
@@ -4,13 +4,14 @@ import pygame.gfxdraw
 from constant import WINDOW_HEIGHT, WINDOW_WIDTH
 from menu_button import MenuButton
 
+
 class Window:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         self.screen.fill((255, 255, 255))
         self.basic_font = pygame.font.Font('Fonts/OpenSans-Regular.ttf', 25)
-        self.buttons = [MenuButton("activities", self.screen, True),
+        self.menu_buttons = [MenuButton("activities", self.screen, True),
                         MenuButton("planer", self.screen, False),
                         MenuButton("food", self.screen, False),
                         MenuButton("sport", self.screen, False),
@@ -25,19 +26,32 @@ class Window:
         menu_h = menu_bar_img.get_height()
         self.screen.blit(menu_bar_img, (0, WINDOW_HEIGHT - menu_h))
 
-        for button in self.buttons:
+        for button in self.menu_buttons:
             button.draw()
 
+    def check_menu_buttons(self, mouse_pos):
+        selected = None
+        for button in self.menu_buttons:
+            if button.pressed(mouse_pos):
+                selected = button
 
+        if selected:
+            for button in self.menu_buttons:
+                if selected == button:
+                    button.active = True
+                else:
+                    button.active = False
 
 
     def run(self):
         self.draw_frame()
         while True:
+            self.screen.fill((255, 255, 255))
+            self.draw_frame()
             pygame.display.update()
-
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit(0)
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     mouse_pos = event.pos
+                    self.check_menu_buttons(mouse_pos)
